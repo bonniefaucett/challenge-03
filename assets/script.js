e0dfa26ab4a72bd735e553a1e7d00315cd64b051
Bonnie Faucett <bonniefaucett@Bonnies-iMac.local>
2023-01-18 08:13:42 -0700

    added password criteria and a few comments

--- assets/script.js
+++ assets/script.js
@@ -1,7 +1,14 @@
 // Assignment Code
+
+// References the id of 'generate' in line 28 of the HTML file
 var generateBtn = document.querySelector("#generate");
 
+// Password criteria
+const passwordCriteria = "abcdefghijlkmnopqrstuvwxyz".split("");
+
 // Write password to the #password input
+
+// Function called by the event listener "generateBtn.addEventListener" upon button click
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
