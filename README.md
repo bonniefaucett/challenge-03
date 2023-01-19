6ce2e5aef32bf9120f52d5f65ab6ef9ae6391dec
Bonnie Faucett <bonniefaucett@Bonnies-iMac.local>
2023-01-18 18:23:34 -0700

    final version to push

---
+++ README.md
@@ -0,0 +1,18 @@
+# Random Password Generator
+
+JavaScript
+
+## My Task
+
+For this challenge, I modified starter code to create an application that enables an employee to generate a random password based on their chosen criteria.
+
+The modified code satisfies the following requirements:
+* The employee is presented with a series of prompts for password criteria.
+* The employee is prompted to choose the length of the password (between 8 and 128 characters) and whether to include lowercase letters, uppercase letters, numerals, and/or special characters.
+* Once the employee answers the prompts, a password consistent with their selections is randomly generated and displayed.
+
+## Screenshot
+![Project Screenshot](/assets/screenshot.png)
+
+## Link To Deployed Application
+https://bonniefaucett.github.io/challenge-03/
--- assets/script.js
+++ assets/script.js
@@ -32,7 +32,7 @@ var generatePassword = function() {
   for (i = 0; i < passwordCriteria.length; i++){
     var charType = randomNumber(1, 4);
 
-    // These conditional statemetns generate random character types based on the various criteria categories that the employee can choose from.
+    // These conditional statements generate random character types based on the various criteria categories that the employee can choose from.
     if (charType === 1 && passwordCriteria.lowercase === 1) {
       newPassword += genLowercase();
       console.log(newPassword);
@@ -49,22 +49,20 @@ var generatePassword = function() {
       newPassword += genSpecial();
       console.log(newPassword);
     }
-    //***if all else fails, decrement i to throw out this iteration through the loop and try again.
+    // Repeat the loop if needed.
     else i--;
     
   }
   return newPassword;
 }
 
-//***This variable generates a password of a random numbers between and including min and max
+// This variable generates a password length between 8 and 128 characters.
 var randomNumber = function(min, max) {
   var value = Math.floor(Math.random() * (max - min + 1) + min);
   return value;
 }
 
-//this function presents the user with a series of prompts to determine what criteria the password 
-//should include. The criteria are stored in a global object to make it easier for all parts of the 
-//program to access the criteria.
+// Employee is presented with a series of prompts to determine what criteria they want in their randomly generated password.
 var criteriaPrompts = function() {
   
   while (passwordCriteria.length < 8 || passwordCriteria.length > 128) {
@@ -75,35 +73,41 @@ var criteriaPrompts = function() {
   }
 
   while (passwordCriteria.lowercase === 0 && passwordCriteria.uppercase === 0 && passwordCriteria.numeric === 0 && passwordCriteria.special === 0) {
-    var lowercaseConfirm = window.confirm("Include lowercase letters in your randomly generated password?");
+    var lowercaseConfirm = window.confirm("Include lowercase letters in your randomly generated password? Press OK for Yes or CANCEL for No.");
     if (lowercaseConfirm) {
       passwordCriteria.lowercase = 1;
     }
-    var uppercaseConfirm = window.confirm("Include uppercase letters in your randomly generated password?");
+    var uppercaseConfirm = window.confirm("Include uppercase letters in your randomly generated password? Press OK for Yes or CANCEL for No.");
     if (uppercaseConfirm) {
       passwordCriteria.uppercase = 1;
     }
-    var numericConfirm = window.confirm("Include numbers in your randomly generated password?");
+    var numericConfirm = window.confirm("Include numbers in your randomly generated password? Press OK for Yes or CANCEL for No.");
     if (numericConfirm) {
       passwordCriteria.numeric = 1;
     }
-    var specialConfirm = window.confirm("Include special characters in your randomly generated password?");
+    var specialConfirm = window.confirm("Include special characters in your randomly generated password? Press OK for Yes or CANCEL for No.");
     if (specialConfirm) {
       passwordCriteria.special = 1;
     }
     if (passwordCriteria.lowercase === 0 && passwordCriteria.uppercase === 0 && passwordCriteria.numeric === 0 && passwordCriteria.special === 0){
-      window.alert ("Please try again! You must choose at least one type of character to include in your randomly generated password.");
+      window.alert ("Try again! You must choose at least one type of character to include in your randomly generated password.");
     }
     console.log(passwordCriteria);
   }
 }
 
-// These variables refer to the ASCII table at https://www.ascii-code.com/ to generate a single character from the appropriate criteria categories.
+// The variables below refer to the ASCII table at https://www.ascii-code.com/ to generate a single character from the appropriate criteria categories. I know CharCode is deprecated and I think I should have used the keyDown event but I could not figure it out by the challenge deadline.
 
 // Character 97 in the ASCII table is Lowercase a and counting consecutively 25 more characters covers the range from Lowercase a to Lowercase z.
 var genLowercase = function() {
   return String.fromCharCode(97 + randomNumber(0, 25));
 }
+
+// I tried to go by the instructor's teaching on how to generate lowercase letters but when I tried the code below, I couldn't get it to work properly.
+// var genLowercase = function() {
+//   return characters = "abcdefghijlkmnopqrstuvwxyz".split("");
+// }
+
 
 // Character 65 in the ASCII table is Uppercase A and counting consecutively 25 more characters covers the range from Uppercase A to Uppercase Z.
 var genUppercase = function() {
@@ -115,7 +119,7 @@ var genNumeric = function() {
   return String.fromCharCode(48 + randomNumber(0, 9));
 }
 
-// Character 58 in the ASCII table is a colon and counting consecutively six more characters includes the special characters colon, semicolon, less than sign, equal sign, greater than sign, question mark, and @ sign. I wasn't sure how to include separate consecutive characters in the ASCII table, e.g., 33-47.
+// Character 58 in the ASCII table is a colon and counting consecutively six more characters includes the special characters colon, semicolon, less than sign, equals sign, greater than sign, question mark, and @ sign. I wasn't sure how to include separate consecutive characters in the ASCII table, e.g., 33-47.
 var genSpecial = function() {
   return String.fromCharCode(58 + randomNumber(0, 6));
 }
